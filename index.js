b45f58eebd540e14edc766408f8b39ad87613d1c
Hasanuzzaman92 <hasanuzzamanmethu@gmail.com>
2022-10-25 01:08:59 +0600

    courses added in api

--- index.js
+++ index.js
@@ -9,10 +9,13 @@ const port = process.env.PORT || 5000;
 const courses = require('./data/data.json');
 
 
-
-
 app.get('/', (req, res) =>{
     res.send('e-learning server is running')
+})
+
+
+app.get('/courses', (req, res) =>{
+    res.send(courses);
 })
 
 
